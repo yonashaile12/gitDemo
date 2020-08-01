8a4ebbea12b61ed4f654a64145ef0edf3b8102e4
yonashaile12 <yonasmesfin@Yonass-MacBook-Air.local>
2020-08-01 11:43:32 -0500

    add PracticeGit.java

---
+++ src/PracticeGit.java
@@ -0,0 +1,12 @@
+public class PracticeGit {
+
+    public static void main(String[] args) {
+
+        int num1 = 25;
+        int num2 = 4;
+        int product = num1 * num2;
+
+
+        System.out.println(product);
+    }
+}
