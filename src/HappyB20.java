6b6dae1a0028d1286bf0d21e0b226eb473e56f0e
yonashaile12 <yonasmesfin@Yonass-MacBook-Air.local>
2020-08-01 10:32:52 -0500

    add HappyB20.java

---
+++ src/HappyB20.java
@@ -0,0 +1,11 @@
+public class HappyB20 {
+
+    public static void main(String[] args) {
+
+        String firstName = "Yonas";
+        String lastName = "Habtu";
+        String str = "Happy B20";
+        System.out.println(firstName+" "+lastName+", "+str);
+
+    }
+}
--- src/Hello_Batch20.java
+++ src/Hello_Batch20.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Hello_Batch20 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello B20, it is my pleasure to be with you!");
+
+
+
+        // added line
+
+    }
 }
+
