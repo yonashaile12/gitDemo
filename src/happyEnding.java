4c70478054d73e976d3a59a44ff4fb7010c61a69
yonashaile12 <yonashaile12@gmail.com>
2020-09-16 15:22:29 -0500

    day54_Class

--- src/happyEnding.java
+++ src/happyEnding.java
@@ -1,2 +1,9 @@
-public class happyending {
+public class happyEnding {
+
+    public static void main(String[] args) {
+        System.out.println("I am confused!");
+
+
+        System.out.println("please work this time!");
+    }
 }
